8474711cf5d9056b0fcb45bddd763e2469ed199a
erock2112 <borenet@gmail.com>
2022-12-26 02:29:00 -0500

    Scale render-image to a maximum size

--- static/modules/elements/render-image.js
+++ static/modules/elements/render-image.js
@@ -3,26 +3,47 @@ import { registerProcessor } from "../processor-registry.js";
 import {TransformerEb, ImageInput} from "./transformer.js";
 
 export class RenderImageEb extends TransformerEb {
+    static maxDisplayWidth = 400;
+    static maxDisplayHeight = 300;
+
     constructor() {
         super("Render Image", [new ImageInput("image")], []);
+        this.displayCanvas = null;
+        this.fullSizeCanvas = null;
+        this.hasImage = false;
         this._preProcess = () => {
-            if (this.canvas) {
-                this.canvas.style.display = "none";
-            }
+            this.fullSizeCanvas = null;
             this.hasImage = false;
         }
         this._process = (image) => {
-            image.draw(this.canvas);
-            this.canvas.style.display = "block";
+            // Draw the image into the full-size, off-screen canvas.
+            const fullSizeCanvas = document.createElement("canvas");
+            image.draw(fullSizeCanvas);
+            this.fullSizeCanvas = fullSizeCanvas;
+
+            // Scale the full size canvas down into the display canvas.
+            const scaleX = RenderImageEb.maxDisplayWidth / fullSizeCanvas.width;
+            const scaleY = RenderImageEb.maxDisplayHeight / fullSizeCanvas.height;
+            let scale = Math.min(scaleX, scaleY);
+            if (scale > 1) {
+                scale = 1;
+            }
+            this.displayCanvas.width = fullSizeCanvas.width * scale;
+            this.displayCanvas.height = fullSizeCanvas.height * scale;
+            const ctx = this.displayCanvas.getContext("2d");
+            ctx.drawImage(fullSizeCanvas, 0, 0, fullSizeCanvas.width, fullSizeCanvas.height, 0, 0, this.displayCanvas.width, this.displayCanvas.height);
+
             this.hasImage = true;
             return [];
         };
         this._renderContent = () => html`
-            <canvas style="display:none"></canvas>
+            <canvas
+                style="visibility:${this.hasImage ? "visible" : "hidden"}"
+                width="${RenderImageEb.maxDisplayWidth}"
+                height="${RenderImageEb.maxDisplayHeight}"
+                ></canvas>
             <button @click="${() => this.download()}" style="visibility:${this.hasImage ? "visible" : "hidden"}">Download</button>
         `;
-        this.canvas = null;
-        this.hasImage = false;
     }
 
     static properties = {
@@ -31,16 +52,16 @@ export class RenderImageEb extends TransformerEb {
 
     updated() {
         super.updated();
-        this.canvas = this.shadowRoot.querySelector("canvas");
+        this.displayCanvas = this.shadowRoot.querySelector("canvas");
     }
 
     download() {
-        if (!this.hasImage) {
+        if (!this.fullSizeCanvas) {
             return;
         }
         const a = document.createElement("a");
         a.download = "edited.jpg";
-        a.href = canvas.toDataURL();
+        a.href = this.fullSizeCanvas.toDataURL();
         a.click();
     }
 }
